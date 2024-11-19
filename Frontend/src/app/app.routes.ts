0ed399dcbc78e992f769f5bf9ca2aa85640ad924
JulesSylMUAMBA <muambamvelejules@gmail.com>
2024-11-19 18:32:27 +0100

    Delete Frontend directory

--- Frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { TodoListPageComponent } from './todo-list-page/todo-list-page.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/todos', pathMatch: 'full' }, // Redirection par défaut
-  { path: 'todos', component: TodoListPageComponent },
-];
